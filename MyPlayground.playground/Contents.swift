0db50d5eccc1c53b4ee72b934c51f81ee8f6d054
Saul Urias <saul_ulises55@hotmail.com>
2016-09-01 22:03:26 -0700

    Primera Sesion del curso

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//***Strings***
+//Asignación implicita
+var str = "Hello, playground"
+
+//Asignación explicita
+var nombre:String = "Saul Urias"
+
+
+//***Int***
+var a:Double = 11
+var b:Double = 10
+
+//Operadores Básicos
+var resultado:Double
+resultado = a+b
+resultado = a-b
+resultado = a*b
+resultado = a/b
+
+//**Double & Float**
+var piDouble:Double = 3.1415
+var piFloat:Float = 3.1415
+
+//Boleanos "Bool"
+var positivo:Bool = true
+var negativo:Bool = false
+
+var x = 10
+var y = 8
+
+var comparacion:Bool = x == y
+
+
+
+
+
