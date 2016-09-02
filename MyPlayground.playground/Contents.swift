ffa7edcb807f2bdc879f6d7e07fb8dd66314bdda
Saul Urias <saul_ulises55@hotmail.com>
2016-09-01 22:11:18 -0700

    Se agregaron nuevos comentarios al proyecto

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,8 @@
 
 import UIKit
 
-//***Strings***
+//***Strings*** - Cadenas de texto
+//Una variable de tipo String siempre debe de ir entre comillas ""
 //Asignación implicita
 var str = "Hello, playground"
 
@@ -10,28 +11,33 @@ var str = "Hello, playground"
 var nombre:String = "Saul Urias"
 
 
-//***Int***
-var a:Double = 11
-var b:Double = 10
+//***Int - Enteros***
+var a:Int = 11
+var b:Int = 10
 
 //Operadores Básicos
-var resultado:Double
+var resultado:Int
 resultado = a+b
 resultado = a-b
 resultado = a*b
 resultado = a/b
-
+/*
+La division de 11/10 es igual a 1.1 pero al ser tipo de dato entero, este se convierte automaticamente en entero y para no tener ese tipo de errores en la division es mejor utilizar un Double o un Floar
+*/
 //**Double & Float**
+//La diferencia entre un double y un float es el espacio en memoria y como desarrolladores nosotros tenemos que elegir el tipo de dato que más nos convenga
 var piDouble:Double = 3.1415
 var piFloat:Float = 3.1415
 
-//Boleanos "Bool"
+//Boleanos "Bool" - Solo pueden almacenar un valor y este debe ser True o False y es muy utilizado para verificaciones
 var positivo:Bool = true
 var negativo:Bool = false
 
 var x = 10
 var y = 8
 
+
+// El operador "==" es para hacer comparaciones
 var comparacion:Bool = x == y
 
 
